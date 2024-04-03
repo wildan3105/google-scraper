5f572dc7d7f40c400d4f865795676c3d888b4b56
wildan3105 <winistein@gmail.com>
2024-04-03 12:40:35 +0700

    feat(homepage): show empty keywords page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,16 +1,16 @@
-// components/Home.tsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CSV from "./CSV";
 import KeywordDetails from "./KeywordDetails";
 
 import "../styles/Home.scss";
+import { fetchKeywords, getKeywordsResponse } from "../services/keywordApis";
 
 interface HomeProps {
   userEmail: string | null;
-  keywords: { id: string; created_at: string; value: string }[];
 }
 
-const Home: React.FC<HomeProps> = ({ userEmail, keywords }) => {
+const Home: React.FC<HomeProps> = ({ userEmail }) => {
+  const [keywords, setKeywords] = useState<getKeywordsResponse[]>([]);
   const [selectedKeyword, setSelectedKeyword] = useState<{
     id: string;
     value: string;
@@ -19,6 +19,16 @@ const Home: React.FC<HomeProps> = ({ userEmail, keywords }) => {
   const handleKeywordClick = (keyword: { id: string; value: string }) => {
     setSelectedKeyword(keyword);
   };
+
+  useEffect(() => {
+    fetchKeywords()
+      .then((response) => {
+        setKeywords(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching keywords:", error);
+      });
+  }, []);
 
   return (
     <div className="homepage">
@@ -33,36 +43,40 @@ const Home: React.FC<HomeProps> = ({ userEmail, keywords }) => {
         <h3 className="keywords-header">Your Keywords:</h3>
         <CSV />
       </div>
-      <div className="keywords">
-        <table className="table table-striped table-bordered">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Created At</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            {keywords.map((keyword) => (
-              <tr key={keyword.id}>
-                <td>{keyword.id}</td>
-                <td>{keyword.created_at}</td>
-                <td>
-                  <button
-                    className="btn btn-link"
-                    onClick={() => handleKeywordClick(keyword)}
-                    data-bs-toggle="modal"
-                    data-bs-target="#keywordModal"
-                    title={`Click to learn more about ${keyword.value}`}
-                  >
-                    {keyword.value}
-                  </button>
-                </td>
+      {keywords.length > 0 ? (
+        <div className="keywords">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Created At</th>
+                <th>Value</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {keywords.map((keyword) => (
+                <tr key={keyword.id}>
+                  <td>{keyword.id}</td>
+                  <td>{keyword.created_at}</td>
+                  <td>
+                    <button
+                      className="btn btn-link"
+                      onClick={() => handleKeywordClick(keyword)}
+                      data-bs-toggle="modal"
+                      data-bs-target="#keywordModal"
+                      title={`Click to learn more about ${keyword.value}`}
+                    >
+                      {keyword.value}
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <p> Nothing to see... Please upload your first CSV</p>
+      )}
       {selectedKeyword && (
         <KeywordDetails
           keywordId={selectedKeyword.id}
