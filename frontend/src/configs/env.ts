e4d6733b20c8387a376d79673426a49b5df9a85b
wildan3105 <winistein@gmail.com>
2024-04-04 16:21:06 +0700

    fix(temp): hardcode backend url

--- frontend/src/configs/env.ts
+++ frontend/src/configs/env.ts
@@ -1,4 +1,5 @@
 export const APP_ENV = import.meta.env.APP_ENV || "local";
 export const BACKEND_URL =
-  import.meta.env.BACKEND_URL || "http://localhost:3000";
+  import.meta.env.BACKEND_URL ||
+  "https://google-scraper-7cabe000fda7.herokuapp.com";
 export const SOCKET_URL = import.meta.env.SOCKET_URL || "http://localhost:2000";
