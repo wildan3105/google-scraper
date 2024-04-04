c542c6bc0315e200a596cea8279d0dc372553699
wildan3105 <winistein@gmail.com>
2024-04-04 20:41:56 +0700

    fix: no need to have fallback on env config frontend

--- frontend/src/configs/env.ts
+++ frontend/src/configs/env.ts
@@ -1,5 +1,3 @@
 export const APP_ENV = import.meta.env.APP_ENV || "local";
-export const BACKEND_URL =
-  import.meta.env.BACKEND_URL ||
-  "https://google-scraper-7cabe000fda7.herokuapp.com";
-export const SOCKET_URL = import.meta.env.SOCKET_URL || "http://localhost:2000";
+export const BACKEND_URL = import.meta.env.BACKEND_URL;
+export const SOCKET_URL = import.meta.env.SOCKET_URL;
