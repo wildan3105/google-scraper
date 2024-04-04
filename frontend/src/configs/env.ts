2b97276c98a46676b1d28b57f0a0949f099ca555
wildan3105 <winistein@gmail.com>
2024-04-04 20:57:02 +0700

    fix: use fallback in src/configs/env.ts

--- frontend/src/configs/env.ts
+++ frontend/src/configs/env.ts
@@ -1,3 +1,7 @@
 export const APP_ENV = import.meta.env.APP_ENV || "local";
-export const BACKEND_URL = import.meta.env.BACKEND_URL;
-export const SOCKET_URL = import.meta.env.SOCKET_URL;
+export const BACKEND_URL =
+  import.meta.env.BACKEND_URL ||
+  "https://google-scraper-7cabe000fda7.herokuapp.com";
+export const SOCKET_URL =
+  import.meta.env.SOCKET_URL ||
+  "https://google-scraper-7cabe000fda7.herokuapp.com";
