6a5c999cdd39ef64ba7c45ed5c453610100b345a
wildan3105 <winistein@gmail.com>
2024-04-05 08:29:05 +0700

    fix: remove importing types

--- frontend/tests/components/Toast.test.tsx
+++ frontend/tests/components/Toast.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import "@types/jest";
+// import "@types/jest";
 import Toast from "../../src/components/Toast";
 
 describe("Toast component", () => {
